27441f7b834927d573d3626d2e3a103560169de1
Alex Delgado <gadc1996@gmail.com>
2021-03-17 19:58:01 -0600

    File to multiple audios

--- tts.py
+++ tts.py
@@ -1,17 +1,20 @@
-from gtts import gTTS, lang
+from gtts import gTTS
 import PyPDF2
 
 ## Quick Settings
 # Name of the book without extension, must be on PWD
-bookname =  input('Book name (Without extension): ')
+#bookname =  input('Book name (Without extension): ')
+bookname =  'redes de computadora'
 
 # Language of the book
 # Run gTTS-cli --all to check for available languages
-language =  input('Book languaje (es, en): ')
+#language =  input('Book languaje (es, en): ')
+language =  'es'
 
 # Page where the file is going to start reading
 # Leave 0 to read all 
-startPage = int(input('Starting page: ') )
+#startPage = int(input('Starting page: ') )
+startPage = 26
 
 ## PDF proccesing
 #Create a pdf file object
@@ -23,7 +26,8 @@ pdf_reader = PyPDF2.PdfFileReader(pdf)
 
 # Count the total number of pages in the document
 pages = pdf_reader.numPages
-endPage = int(input('Ending page(blank for all, max {}): '.format(pages)))
+endPage = 27
+#endPage = int(input('Ending page(blank for all, max {}): '.format(pages)))
 
 if endPage == "":
     endPage = pages
@@ -36,15 +40,23 @@ textList = []
 for pageNumber in range(startPage , endPage):
     try:
         page = pdf_reader.getPage(pageNumber)
-        textList.append(page.extractText())
+
+        #Convert multiple lines to a single string
+        textString = page.extractText()
+
+        #Remove Newline characters
+        textString = textString.replace('\n', '')
+
+        #Remove Newline characters
+        textString = textString.replace('-', '')
+
+        #Convert text to audio
+        audio = gTTS(text=textString, lang=language, slow=False)
+
+        #Save audio as mp3 file
+        audio.save(bookname+"-page-{}".format(pageNumber)+'.mp3')
+        print(bookname+"-page-{}".format(pageNumber)+'.mp3 Created')
+
     except:
-        pass
+        print(bookname+"-page-{}".format(pageNumber)+'.mp3 Not Created')
 
-#Convert multiple lines to a single string
-textString = " ".join(textList)
-
-#Convert text to audio
-audio = gTTS(text=textString, lang=language, slow=False)
-
-#Save audio as mp3 file
-audio.save(bookname+'.mp3')
